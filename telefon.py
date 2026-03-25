10e13e0930e4a5a7363e2a62f86ba63bb5aa37a2
darkest453 <robin.ostmark@elev.ga.dbgy.se>
2026-03-25 09:42:16 +0100

    spara kontakter

--- telefon.py
+++ telefon.py
@@ -1,1 +1,11 @@
-#hadsadadas
+import json
+import os
+
+DATAFIL = "Kontakter.json"
+
+def ladda_kontakter():
+    if os.path.exists(DATAFIL):
+        with open(DATAFIL, "r", encoding="utf-8") as f:
+            return json.load(f)
+        
+    return{}
